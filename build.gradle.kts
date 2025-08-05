c391e1e8ecab92e2bc149c3052a93f2f84848eb7
xubo18639520787-afk <xubo18639520787@gmail.com>
2025-08-05 17:32:20 +0800

    修复kotlin-kapt插件版本号问题

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
-    id("kotlin-kapt") apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.10" apply false
 }
